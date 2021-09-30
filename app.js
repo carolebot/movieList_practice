794dd55bf83a24da389eef36a713962c828897fc
carolebot <carol.tang79@gmail.com>
2021-09-30 16:46:35 +0800

    add detail show page using params router

--- app.js
+++ app.js
@@ -6,26 +6,31 @@ const exphbs = require('express-handlebars')
 const movieList = require('./movies.json').results
 
 // use hbs 簡寫記得後面加上extname:
-app.engine('hbs', exphbs({defaultLayout:'main', extname:'.hbs'}))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 // set static files
 app.use(express.static('public'))
 
 // body-parser
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 // set router 
 app.get('/', (req, res) => {
-  res.render('index', { movies: movieList})
+  res.render('index', { movies: movieList })
 })
 
 app.get('/movies/:id', (req, res) => {
-  const id = req.params.id
-  res.render('index')
+  const id = req.params.id //字串
+  // find 只有一個結果 適合用在id
+  const movie = movieList.find((movie) => {
+    return movie.id.toString() === id
+  })
+
+  res.render('show', { movie })
 })
 
 // server start listen 
-app.listen(port, ()=> {
+app.listen(port, () => {
   console.log(`exp is running on http://localhost:${port}`)
 })

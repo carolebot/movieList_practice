7fa270ba64ad2bf48d9ad3a855f2553bc81c26f7
carolebot <carol.tang79@gmail.com>
2021-09-30 15:05:58 +0800

    proj init

---
+++ app.js
@@ -0,0 +1,30 @@
+// set express, port, exphbs
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+
+// use hbs 簡寫記得後面加上extname:
+app.engine('hbs', exphbs({defaultLayout:'main', extname:'.hbs'}))
+app.set('view engine', 'hbs')
+
+// set static files
+app.use(express.static('public'))
+
+// body-parser
+app.use(express.urlencoded({extended: true}))
+
+// set router 
+app.get('/', (req, res) => {
+  res.send('this is homepage')
+})
+
+app.get('/movies/:id', (req, res) => {
+  const id = req.params.id
+  res.send(`this is movie id ${id} page`)
+})
+
+// server start listen 
+app.listen(port, ()=> {
+  console.log(`exp is running on http://localhost:${port}`)
+})
